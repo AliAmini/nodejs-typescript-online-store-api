eee1848bf2ca3dd61d6898c1097bcee0fe15b587
Ali Amini <aliamini.af@gmail.com>
2022-06-04 18:08:42 +0430

    complete logic of login route and generating jwt

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -1,12 +1,26 @@
+import { comparePassword, createJWT, validateJWT } from "@helpers/Auth.helper";
+import User, { IUser } from "@models/User/User.model";
+
 class AuthService {
   constructor() {}
 
-  async loginUser(email: string, password: string): Promise<{token: string}> {
-
-    let token: string = '';
-    return {
-      token
+  async loginUser(email: string, password: string): Promise<string> {
+    const foundUser = await User.findOne({email: email}).lean().exec();
+    if(!foundUser) {
+      throw new Error("User is not found");
+    } else {
+      console.log('Password, hash', password, foundUser.password);
     }
+
+    const isCorrectPassword = comparePassword(password, foundUser.password);
+    if(!isCorrectPassword) {
+      throw new Error("Entered email or password is not correct.");
+    }
+
+
+    let token: string = createJWT(foundUser);
+
+    return token;
   }
 }
 
